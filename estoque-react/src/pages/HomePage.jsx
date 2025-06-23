50338e9b8ce57839531550b7d598218bdcb052a2
Ludson Almeida <ludson.bsa@gmail.com>
2025-06-23 09:38:06 -0300

    asd

--- estoque-react/src/pages/HomePage.jsx
+++ estoque-react/src/pages/HomePage.jsx
@@ -84,7 +84,7 @@ export default function HomePage() {
   const handleReservationSubmit = async data => {
     try {
       const response = await fetch(
-        `${API_BASE}/api/reservations`, {
+        `${API_BASE}/reservations`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
