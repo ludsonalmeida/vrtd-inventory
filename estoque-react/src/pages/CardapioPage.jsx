811373006fc84094eb988ecb8982a96aeab3ed2c
Ludson Almeida <ludson.bsa@gmail.com>
2025-06-23 12:20:45 -0300

    cardapio com pixel

--- estoque-react/src/pages/CardapioPage.jsx
+++ estoque-react/src/pages/CardapioPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { Box, Typography } from '@mui/material';
+import PixelLoader from '../components/PixelLoader';
 
 export default function CardapioPage() {
   useEffect(() => {
@@ -9,19 +10,23 @@ export default function CardapioPage() {
   }, []);
 
   return (
-    <Box component="main" sx={{ p: 4, maxWidth: 1200, mx: 'auto' }}>
-      <Typography variant="h4" component="h1" gutterBottom>
-        Nosso Cardápio
-      </Typography>
-      {/* Conteúdo do cardápio: PDF embutido ou lista de itens */}
-      <Box
-        component="iframe"
-        src="https://porks.nyc3.cdn.digitaloceanspaces.com/cardapio.pdf"
-        width="100%"
-        height="calc(100vh - 160px)"
-        sx={{ border: 0 }}
-        title="Cardápio Completo"
-      />
-    </Box>
+    <>
+      {/* Carrega e inicializa o Meta Pixel */}
+      <PixelLoader />
+      <Box component="main" sx={{ p: 4, maxWidth: 1200, mx: 'auto' }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Nosso Cardápio
+        </Typography>
+        {/* Exibe o cardápio em tela cheia */}
+        <Box
+          component="iframe"
+          src="https://porks.nyc3.cdn.digitaloceanspaces.com/cardapio.pdf"
+          width="100%"
+          height="calc(100vh - 160px)"
+          sx={{ border: 0 }}
+          title="Cardápio Completo"
+        />
+      </Box>
+    </>
   );
 }
